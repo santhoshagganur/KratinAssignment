3cf093c2a837fc16d9a15f595af43032141c3a15
santhoshagganur <agganursanthosh@gmail.com>
2023-08-12 10:10:49 +0000

    almost done

--- src/components/TabletsRemainders/index.js
+++ src/components/TabletsRemainders/index.js
@@ -10,8 +10,20 @@ class TabletsRemainders extends Component {
 
   getTabletRemainder = () => {
     const {time} = this.state
-    if (time === '12:00') {
-      alert('in progress')
+    if (time === '12:30') {
+      alert(
+        'You can take Glimepiride, Gliclazide IR tablets. We wish a speedy recovery.',
+      )
+    } else if (time === '13:00') {
+      alert('Eat a healthy food.')
+    } else if (time === '13:10' || time === '13:15') {
+      alert(
+        'You can take Gliclazide XR, Metformin IR/XR tablets. We wish a speedy recovery.',
+      )
+    } else if (time === '18:00') {
+      alert('You can take LIRAGLUTIDE. We wish a speedy recovery.')
+    } else if (time === '11:00' || time === '19:00') {
+      alert('You need to take a INSULIN. We wish a speedy recovery.')
     }
   }
 
